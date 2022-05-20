9a65bb0051933902ce516375a7a154338b62ac98
daniela-cardenas-b <daniela.cardenas@comunidad.unam.mx>
2022-05-19 23:46:41 -0500

    Añadiendo js

---
+++ dynamics/js/juegos.js
@@ -0,0 +1,13 @@
+const divCuadro = document.getElementById(contenedor); 
+
+
+
+
+//hasta tener el html funcional
+divCuadro.addEventListener("mouseover", (evento) =>{
+    console.log("Dentro del evento"); 
+    divCuadro.innerHTML="<div class='juego'><strong>Nombre"+"Aquí agregar desde JSON"+"</strong></div>"; 
+    divCuadro.innerHTML+="<div class='juego'><strong>Descripción"+"Aquí datos JSON"+"</strong></div>"; 
+    divCuadro.innerHTML+="<div class='juego'>"+"<button class='boton-azul'></button>"+"Referencia boton jugar"+"</strong></div>"; 
+    divCuadro.style.display = "flex"; 
+}); 
---
+++ dynamics/js/tablero.js
@@ -0,0 +1,9 @@
+const divContenedor = document.getElementById("contenedor")
+
+//si los datosJSON son true //para
+'<div class="materia" id="mate">'+'<div><h4>Aquí agregar nombre materia</h4><img src="../Imgs/AquíRutaDeImagen.ext" alt="Imagen de la materia" width="200" height="180" /> </div>';  
+'<div class="tareas" id="tareas">'+'Tareas pendientes:<ul><a><li>En esta lista agregar tareas</li></a><a><li>Tarea 2</li></a><a><li>Tarea 3</li> </a></ul></div></div>'; 
+
+//recepción de datos con fetch
+
+//estoy trabajando en este :) 
